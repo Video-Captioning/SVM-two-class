830cc5b72cac2b04ae91991943e10e41cf8c454a
Karl Edwards <edwards.karl@ymail.com>
2018-04-05 09:21:29 -0700

    Update SVM_two_class_functions.R

--- SVM_two_class_functions.R
+++ SVM_two_class_functions.R
@@ -1,9 +1,11 @@
-
 # EXAMPLE USAGE:
-#    microbenchmark::microbenchmark( (results <- update( './disc05/cells4_bins6/' )), times=1L )
+#    microbenchmark::microbenchmark(
+#         (results <- update( './disc05/cells4_bins6/' ))
+#       , times=1L
+#     )
 
 # --- Configuration
-DATA_PATH <- '/Users/Karl/Dropbox/Projects/Video-Captioning/'
+DATA_PATH <- '/path/to/Projects/Video-Captioning/'
 
 # --- Deeper Configuration --- less likely to change
 
@@ -22,31 +24,34 @@ CFG <- dplyr::tribble(
 #     F U N C T I O N S
 # -----------------------------------------------
 
-# Load and label the data
+# Load and label the data for one row of CFG
 load_and_label <- function( y, fname ){
   data <- read.csv( file=fname, header=FALSE, sep=',', as.is=TRUE, row.names=1 )
   data$y <- y
   data
 }
 
+# Optimize cost parameter, C,
+# by training an SVM classifier using each of the
+# C-values given in input, range_lists
 optimize_cost <- function( range_lists, test_data ){
   cat( sprintf( 'Finding optimal cost...\n' ))
-  # find optimal cost of confusion_matrixification
   e1071::tune(
       e1071::svm
     , y~.
-    , data = test_data
+    , data   = test_data
     , kernel = 'linear'
     , ranges = range_lists
   )
 }
 
+# Create a confusion matrix
 get_confusion <- function( thefit, thedata, y ){
-  # Create a table of mis-classified observations
   predictions <- ifelse( predict( thefit, thedata ) > 0.50, 1, 0 )
   confusion_matrix <- table( predict = predictions, truth = y )
 }
 
+# Calculate the effectiveness of the classifier
 evaluate_fit <- function( confusion_matrix ){
   TN <- confusion_matrix[ 1, 1 ]
   FN <- confusion_matrix[ 1, 2 ]
@@ -63,19 +68,22 @@ evaluate_fit <- function( confusion_matrix ){
   )
 }
 
+# Save some stuff, someplace
 save_as <- function( data, fname, path = BASE_PATH ){
-  X <- data.frame( data )
-  names( X ) <- NULL
+  df <- data.frame( data )
+  names( df ) <- NULL
   write.csv(
-      X
+      df
     , file = paste0( path, fname )
     , quote = FALSE, row.names = FALSE
   )
 }
 
-add_row <- function( X, descr, value ){
-  new_index <- 1 + nrow( X )
-  X[ new_index, 'Descr' ] <- descr
-  X[ new_index, 'Value' ] <- value
-  X
+# Add a row of inforation to an existing data frame,
+# populating descr[iption] and value in the first two columns
+add_row <- function( df, descr, value ){
+  new_index <- 1 + nrow( df )
+  df[ new_index, 1 ] <- descr
+  df[ new_index, 2 ] <- value
+  df
 }
