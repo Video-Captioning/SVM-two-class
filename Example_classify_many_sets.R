a0eb4ea0826333f3b5b462dd281179d2e770a662
Karl Edwards <edwards.karl@ymail.com>
2018-04-05 13:10:10 -0700

    Update and rename SVM_classify_many_sets.R to Example_classify_many_sets.R

--- Example_classify_many_sets.R
+++ Example_classify_many_sets.R
@@ -10,18 +10,27 @@ libraries %>%
 
 # ----- Describe which data sets to process -------------
 BATCHES <- list(
+  
     'disc40/cells4_bins9/'
   , 'disc40/cells5_bins9/'
   , 'disc40/cells6_bins9/'
   , 'disc40/cells7_bins9/'
   , 'disc40/cells8_bins9/'
   , 'disc40/cells9_bins9/'
+  
   , 'disc50/cells4_bins9/'
   , 'disc50/cells5_bins9/'
   , 'disc50/cells6_bins9/'
   , 'disc50/cells7_bins9/'
   , 'disc50/cells8_bins9/'
   , 'disc50/cells9_bins9/'
+  
+  , 'disc60/cells4_bins9/'
+  , 'disc60/cells5_bins9/'
+  , 'disc60/cells6_bins9/'
+  , 'disc60/cells7_bins9/'
+  , 'disc60/cells8_bins9/'
+  , 'disc60/cells9_bins9/'
 )
 
 # ----- Process the data sets ----------------------------
